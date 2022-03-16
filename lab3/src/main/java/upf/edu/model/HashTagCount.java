9a0c2ef88609e343facd31b36059957d5fc95e72
Hong-ming <37633722+homexiang@users.noreply.github.com>
2022-03-16 19:54:29 +0100

    dynamoDB (faltan testeos)

--- lab3/src/main/java/upf/edu/model/HashTagCount.java
+++ lab3/src/main/java/upf/edu/model/HashTagCount.java
@@ -8,6 +8,18 @@ public final class HashTagCount {
   final String hashTag;
   final String lang;
   final Long count;
+  
+  public String getHashTag() {
+		return hashTag;
+	}
+
+  public String getLang() {
+		return lang;
+	}
+	
+	public Long getCount() {
+		return count;
+	}
 
   public HashTagCount(String hashTag, String lang, Long count) {
     this.hashTag = hashTag;
--- lab3/src/main/java/upf/edu/storage/DynamoHashTagRepository.java
+++ lab3/src/main/java/upf/edu/storage/DynamoHashTagRepository.java
@@ -1,22 +1,116 @@
 package upf.edu.storage;
 
+import twitter4j.HashtagEntity;
 import twitter4j.Status;
+import upf.edu.model.HashTagCompare;
 import upf.edu.model.HashTagCount;
+
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
 import java.io.Serializable;
 import java.util.*;
 
 public class DynamoHashTagRepository implements IHashtagRepository, Serializable {
+	
+	final static String endpoint = "dynamodb.us-east-1.amazonaws.com";
+	final static String region = "us-east-1";
+	final static String tableName = "LSDS2020-TwitterHashtags";
 
+	final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+			.withEndpointConfiguration(
+					new AwsClientBuilder.EndpointConfiguration(endpoint, region)
+			).withCredentials(new ProfileCredentialsProvider())
+			.build();
+
+	final DynamoDB dynamoDB = new DynamoDB(client);
+	final Table dynamoDBTable = dynamoDB.getTable(tableName);
+		
   @Override
   public void write(Status tweet) {
-    // IMPLEMENT ME
+	  HashtagEntity[] entities = tweet.getHashtagEntities();
+	    if (entities != null) {
+	    	for (HashtagEntity he : entities) {
+	    		if (he != null) {
+  				String hashtag = he.getText().toString();
+  				String lang = tweet.getLang().toString();
+  				Long tweetId = tweet.getId();
+  				// Try to update existing Hashtag
+  				try {
+	    				UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+	    						.withPrimaryKey("hashtag", hashtag, "language", lang)
+	    		                .withUpdateExpression("set myCounter=myCounter+:b, tweetsId=list_append(:a,tweetsId)")
+	    		                .withValueMap(new ValueMap().withNumber(":b",1).withList(":a", tweetId))
+	    		                .withReturnValues(ReturnValue.UPDATED_NEW);
+	
+	    				UpdateItemOutcome outcome = dynamoDBTable.updateItem(updateItemSpec);
+	    				
+	    				System.out.println("Update succeeded:\n" + outcome.getUpdateItemResult());
+  				} catch(Exception e) {
+  					// Hashtag not exists, so add a new Item on the DB
+	    				System.out.println("Adding new item");
+	    				
+	    				// Creates the Hashtag Item
+	    				Item item = new Item()
+	    						.withPrimaryKey("hashtag", hashtag, "language", lang)
+	    						.withNumber("myCounter", 1)
+	                            .withList("tweetsId", Arrays.asList(tweetId));
+	    				
+	    				// Add the Item on DB
+  					dynamoDBTable.putItem(item);
+  				}
+	    		}
+	    	}
+	    }
   }
-
+  
+  @SuppressWarnings("unchecked")
   @Override
   public List<HashTagCount> readTop10(String lang) {
-    // IMPLEMENT ME
-    return Collections.emptyList();
+	  ArrayList<HashTagCount> list = new ArrayList<HashTagCount>();
+      ArrayList<HashTagCount> top10 = new ArrayList<HashTagCount>();
+
+      // Scanning whole DB, and get interesting attributes
+      ScanRequest scanRequest = new ScanRequest()
+              .withTableName(tableName)
+              .withAttributesToGet("hashtag","language","myCounter");
+      ScanResult result = client.scan(scanRequest);
+
+      // Parse the Items to get the correct value
+      for (Map<String, AttributeValue> item : result.getItems()) {
+            String counter = item.values().toArray()[0].toString().split("[ ]")[1].split("[,]")[0];
+            String language = item.values().toArray()[1].toString().split("[ ]")[1].split("[,]")[0];
+            String hashtag = item.values().toArray()[2].toString().split("[ ]")[1].split("[,]")[0];
+            
+            // Filter by language and add to a list
+            if(language.equals(lang)){
+              HashTagCount h = new HashTagCount(hashtag, language, Long.parseLong(counter));
+              list.add(h);
+            }
+      }
+      
+      //Sorting the hashtags using the java comparator class we created.
+      Collections.sort(list,new HashTagCompare());
+      
+      // Add the TOP 10 hashtags (if there are).
+      @SuppressWarnings("rawtypes")
+      Iterator itr=list.iterator();
+      int i=0;
+      while(itr.hasNext() && i<10){
+              top10.add((HashTagCount)itr.next());
+              i++;
+      }
+      
+      return top10;
   }
 
 }
